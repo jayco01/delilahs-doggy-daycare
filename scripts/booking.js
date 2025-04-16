018761ac248399a195bf15549ff168f68a05a5ab
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-04-15 18:44:10 -0600

    added recalccost function and added if no days are clicked statement in both the full and half day function

--- scripts/booking.js
+++ scripts/booking.js
@@ -23,71 +23,65 @@ const clearButton = document.getElementById("clear-button");
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
-
-function addMonday() {
+function toggleMonday() {
     if (monday.classList.contains("clicked")) {
-        monday.classList.remove("clicked");
-        costSum -= costPerDay;
-        calculatedCost.innerHTML = costSum;
+      monday.classList.remove("clicked");
+      costSum -= costPerDay;
     } else {
-        monday.classList.add("clicked");
-        costSum += costPerDay;
-        calculatedCost.innerHTML = costSum;
+      monday.classList.add("clicked");
+      costSum += costPerDay;
     }
-}
-monday.addEventListener("click",addMonday)
-
-function addTeusday() {
-    if (teusday.classList.contains("clicked")) {
-        teusday.classList.remove("clicked");
-        costSum -= costPerDay;
-        calculatedCost.innerHTML = costSum;
+    recalcCost();
+  }
+  monday.addEventListener("click", toggleMonday);
+  
+  function toggleTuesday() {
+    if (tuesday.classList.contains("clicked")) {
+      tuesday.classList.remove("clicked");
+      costSum -= costPerDay;
     } else {
-        teusday.classList.add("clicked");
-        costSum += costPerDay;
-        calculatedCost.innerHTML = costSum;
+      tuesday.classList.add("clicked");
+      costSum += costPerDay;
     }
-}
-teusday.addEventListener("click",addTeusday)
-
-function addWednesday() {
+    recalcCost();
+  }
+  tuesday.addEventListener("click", toggleTuesday);
+  
+  function toggleWednesday() {
     if (wednesday.classList.contains("clicked")) {
-        wednesday.classList.remove("clicked");
-        costSum -= costPerDay;
-        calculatedCost.innerHTML = costSum;
+      wednesday.classList.remove("clicked");
+      costSum -= costPerDay;
     } else {
-        wednesday.classList.add("clicked");
-        costSum += costPerDay;
-        calculatedCost.innerHTML = costSum;
+      wednesday.classList.add("clicked");
+      costSum += costPerDay;
     }
-}
-wednesday.addEventListener("click",addWednesday)
-
-function addThursday() {
+    recalcCost();
+  }
+  wednesday.addEventListener("click", toggleWednesday);
+  
+  function toggleThursday() {
     if (thursday.classList.contains("clicked")) {
-        thursday.classList.remove("clicked");
-        costSum -= costPerDay;
-        calculatedCost.innerHTML = costSum;
+      thursday.classList.remove("clicked");
+      costSum -= costPerDay;
     } else {
-        thursday.classList.add("clicked");
-        costSum += costPerDay;
-        calculatedCost.innerHTML = costSum;
+      thursday.classList.add("clicked");
+      costSum += costPerDay;
     }
-}
-thursday.addEventListener("click",addThursday)
-
-function addFriday() {
+    recalcCost();
+  }
+  thursday.addEventListener("click", toggleThursday);
+  
+  function toggleFriday() {
     if (friday.classList.contains("clicked")) {
-        friday.classList.remove("clicked");
-        costSum -= costPerDay;
-        calculatedCost.innerHTML = costSum;
+      friday.classList.remove("clicked");
+      costSum -= costPerDay;
     } else {
-        friday.classList.add("clicked");
-        costSum += costPerDay;
-        calculatedCost.innerHTML = costSum;
+      friday.classList.add("clicked");
+      costSum += costPerDay;
     }
-}
-friday.addEventListener("click",addFriday)
+    recalcCost();
+  }
+  friday.addEventListener("click", toggleFriday);
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
@@ -98,9 +92,9 @@ function resetTotal() {
     wednesday.classList.remove("clicked");
     thursday.classList.remove("clicked");
     friday.classList.remove("clicked");
-    calculatedCost.innerHTML = 0;
+    costSum = 0;
+    recalcCost();  
 }
-
 clearButton.addEventListener("click",resetTotal)
 
 
@@ -109,36 +103,53 @@ clearButton.addEventListener("click",resetTotal)
 
 function calculateHalfDay() {
     if (!half.classList.contains("clicked")) {
-        half.classList.add("clicked");
-        full.classList.remove("clicked");
-        costPerDay = 20;
-        if (calculatedCost != ""){
-        costSum  = (costSum/35)*costPerDay;
-        calculatedCost.innerHTML = costSum;
-        }
+      half.classList.add("clicked");
+      full.classList.remove("clicked");
+      costPerDay = 20;
+      if (
+        monday.classList.contains("clicked") ||
+        tuesday.classList.contains("clicked") ||
+        wednesday.classList.contains("clicked") ||
+        thursday.classList.contains("clicked") ||
+        friday.classList.contains("clicked")
+      ) {
+        costSum = (costSum / 35) * costPerDay;
+      } else {
+        costSum = 0;
+      }
+      calculatedCost.innerHTML = Math.round(costSum);
     }
-}
-
-half.addEventListener("click",calculateHalfDay)
+  }
+  half.addEventListener("click",calculateHalfDay)
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
 function calculateFullDay() {
     if (!full.classList.contains("clicked")) {
-        full.classList.add("clicked");
-        half.classList.remove("clicked");
-        costPerDay = 35;
-        costSum  = (costSum/20)*costPerDay;
-        calculatedCost.innerHTML = costSum;
+      full.classList.add("clicked");
+      half.classList.remove("clicked");
+      costPerDay = 35;
+  
+      if (
+        monday.classList.contains("clicked") ||
+        tuesday.classList.contains("clicked") ||
+        wednesday.classList.contains("clicked") ||
+        thursday.classList.contains("clicked") ||
+        friday.classList.contains("clicked")
+      ) {
+        costSum = (costSum / 20) * costPerDay;
+      } else {
+        costSum = 0;
+      }
+      calculatedCost.innerHTML = Math.round(costSum);
     }
-}
-
-full.addEventListener("click",calculateFullDay)
-
-
+  }
+  full.addEventListener("click",calculateFullDay)
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
-
+function recalcCost() {
+    calculatedCost.innerHTML = costSum; 
+  }
